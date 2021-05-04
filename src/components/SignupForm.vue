14068420c5130d32dc885086284e718068237145
Rabindra Dahal <radaha@ttu.ee>
2021-05-04 03:07:07 +0300

    Form input manipulation and adding skills using input tag

---
+++ src/components/SignupForm.vue
@@ -0,0 +1,96 @@
+<template>
+  <form>
+      <label for="email">Email:</label>
+      <input type="email" v-model="email" name="email" id="email" required />
+      
+      <label for="password">Password:</label>
+      <input type="password" v-model="password" name="password" id="password" required />
+      
+      <label for="role">Role</label>
+      <select v-model="role" name="role" id="role">
+          <option value="developer"> Web Developer</option>
+          <option value="designer"> Web Designer</option>
+      </select>
+      <label for="tempskill"></label>
+      <input type="text" name="tempskill" id="tempskill" v-model="tempSkill" @keyup.alt="addSkill" />
+      <div v-for="skill in skills" :key="skill" class="skill">
+       {{skill}}
+      </div>
+
+      <div class="terms">
+          <input type="checkbox" name="term" id="term" v-model="terms" required />
+          <label for="term">Accept terms and conditions</label>
+      </div>
+
+      
+  </form>
+  <p>Email: {{ email }}</p>
+  <p>Password: {{ password }} </p>
+  <p>Role: {{ role }} </p>
+  <p>Terms accepted: {{ terms }} </p>
+  <p>Skills: {{ skills }} </p>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            email: '',
+            password: '',
+            role: 'designer',
+            terms: false,
+            tempSkill: '',
+            skills: []
+        }
+    },
+    methods: {
+        addSkill(e){
+            if(e.key === ',' && this.tempSkill){
+                if(!this.skills.includes(this.tempSkill)){
+                    this.skills.push(this.tempSkill)
+                }
+                this.tempSkill = ''
+            }
+        }
+    },
+
+}
+</script>
+
+<style>
+form{
+    max-width: 420px;
+    margin: 30px auto;
+    background: white;
+    text-align: left;
+    padding: 40px;
+    border-radius: 10px;
+}
+label{
+    color: #aaa;
+    display: inline-block;
+    margin: 25px 0 15px;
+    font-size: 0.6em;
+    text-transform: uppercase;
+    letter-spacing: 1px;
+    font-weight: bold;
+}
+input, select{
+    display: block;
+    padding: 10px 6px;
+    width: 100%;
+    box-sizing: border-box;
+    border: none;
+    border-bottom: 1px solid #ddd;
+    color: #555;
+
+}
+input[type="checkbox"]{
+    display: inline-block;
+    width: 16px;
+    margin: 0 10px 0 0;
+    position: relative;
+    top: 2px;
+}
+
+</style>
